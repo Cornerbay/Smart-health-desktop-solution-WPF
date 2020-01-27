b42d177680476a5bbbc8a6cacb5e40d68e436855
Cornerbay <46921134+Cornerbay@users.noreply.github.com>
2020-01-27 12:25:34 +0100

    Testing database connection in main window

--- Smart health desktop solution WPF/MainWindow.xaml.cs
+++ Smart health desktop solution WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace Smart_health_desktop_solution_WPF
 {
@@ -23,6 +25,21 @@ namespace Smart_health_desktop_solution_WPF
         public MainWindow()
         {
             InitializeComponent();
+
+            string connectionString = "SERVER=smart-health-solution.mysql.database.azure.com;DATABASE=app2000;UID=designerkaktus@smart-health-solution;PASSWORD=Vieralleveldigsunne1;";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            MySqlCommand cmd = new MySqlCommand("select*from patient", connection);
+            connection.Open();
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            connection.Close();
+
+            dtGrid.DataContext = dt;
+
         }
+
+
     }
 }
